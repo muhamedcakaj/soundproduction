85d822161a1dd93ccff796952f4c8114adbeaeb7
muhamedcakaj <muhamedcakaj57@gmail.com>
2026-06-29 01:39:14 +0200

    Adding live url for cross origin

--- src/main/java/Sound/Production/Sound/Production/SecurityConfig.java
+++ src/main/java/Sound/Production/Sound/Production/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://sound-production.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         return source -> config;
